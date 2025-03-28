7415ac00ac7447bc6ed9dea8773181846808ab9a
Florian Sebal <fsebal91@gmail.com>
2025-03-28 17:50:33 +0100

    correction du shéma cartoon

--- src/schemas/cartoon.schema.ts
+++ src/schemas/cartoon.schema.ts
@@ -1,12 +1,12 @@
 export const Cartoon = `{
-    id: Number;
-    name: String;
-    description: String;
-    nb_of_episodes: Number;
-    nb_of_seasons: Number;
-    genres: String[];
-    realisator: String;
-    author: String;
-    ft_diffusion: String;
-    personnages: Personnage[];
+    id: ID
+    name: String
+    description: String
+    nb_of_episodes: Int
+    nb_of_seasons: Int
+    genres: [String]
+    realisator: String
+    author: String
+    ft_diffusion: String
+    personnages: [Personnage]
 }`;
