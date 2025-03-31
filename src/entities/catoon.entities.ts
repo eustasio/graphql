884b28d87e75de679436123575ca3543f27061fd
Florian Sebal <fsebal91@gmail.com>
2025-03-31 11:58:30 +0200

    ajout de l'entité cartoon

---
+++ src/entities/catoon.entities.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Cartoon {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  nb_of_episodes: number;
+
+  @Column()
+  nb_of_seasons: number;
+
+  @Column()
+  realisator: string;
+
+  @Column()
+  author: string;
+
+  @Column()
+  ft_diffusion: string;
+}
